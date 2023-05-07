08ba1f93d3fcb4df238f08be1b405c81d4f0b5e1
landrynho17 <130696940+landrynho17@users.noreply.github.com>
2023-05-07 23:16:12 +0000

    We do add mutiply

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const sum = require("./lib/sum");
 const subtract = require("./lib/subtract");
+const multiply = require("./lib/multiply")
 
-console.log(sum(1,2));
-console.log(subtract(1,2));
+// console.log(sum(1,2));
+// console.log(subtract(1,2));
+// console.log(multiply(2,2))
+module.exports = {sum,subtract,multiply};
